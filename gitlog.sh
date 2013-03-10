374f7a8134a3b37de7ec721951360245b00dc522
dreamyguy <i@dreamyguy.com>
2013-03-10 12:40:35 +0100

    Rearrange 'awk' positioning of fields for the first commit, so it's comparable to other commits

--- gitlog.sh
+++ gitlog.sh
@@ -5,20 +5,38 @@ git log --all --no-merges --shortstat --pretty=format:'commit_hash %H commit_has
 	print "commit_hash_abbreviated " $4;
 	print "tree_hash " $6;
 	print "tree_hash_abbreviated " $8;
-	print "parent_hashes " $10;
-	print "parent_hashes_abbreviated " $12;
-	print "author_email " $14;
-	print "date_day_week " $16;
-	print "date_month_name " $17;
-	print "date_month_day " $18;
-	print "date_hour " $19;
-	print "date_year " $20;
-	print "date_hour_gmt " $21;
-	print "author_date_unix_timestamp " $23;
-	print "date_iso_8601 " $25;
-	print "committer_email " $29;
-	print "files_changed " $30;
-	print "insertions " $33;
-	print "deletions " $35;
+    if ( count == 1 ) {
+		print "parent_hashes " "";
+		print "parent_hashes_abbreviated " "";
+		print "author_email " $12;
+		print "date_day_week " $14;
+		print "date_month_name " $15;
+		print "date_month_day " $16;
+		print "date_hour " $17;
+		print "date_year " $18;
+		print "date_hour_gmt " $19;
+		print "author_date_unix_timestamp " $21;
+		print "date_iso_8601 " $23;
+		print "committer_email " $27;
+		print "files_changed " $28;
+		print "insertions " $31;
+		print "deletions " $33;
+    } else {
+		print "parent_hashes " $10;
+		print "parent_hashes_abbreviated " $12;
+		print "author_email " $14;
+		print "date_day_week " $16;
+		print "date_month_name " $17;
+		print "date_month_day " $18;
+		print "date_hour " $19;
+		print "date_year " $20;
+		print "date_hour_gmt " $21;
+		print "author_date_unix_timestamp " $23;
+		print "date_iso_8601 " $25;
+		print "committer_email " $29;
+		print "files_changed " $30;
+		print "insertions " $33;
+		print "deletions " $35;
+	}
 	print "";
 }' > gitlog.json
