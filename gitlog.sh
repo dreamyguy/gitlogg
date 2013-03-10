8e80d82ead072fb5c0151566d1fe10ea41406807
dreamyguy <i@dreamyguy.com>
2013-03-10 13:47:22 +0100

    Second attempt at JSON format, this time with the aid of 'awk' variables

--- gitlog.sh
+++ gitlog.sh
@@ -1,44 +1,53 @@
-git log --all --no-merges --shortstat --pretty=format:'commit_hash %H commit_hash_abbreviated %h tree_hash %T tree_hash_abbreviated %t parent_hashes %P parent_hashes_abbreviated %p author_email %ae author_date %ad author_date_unix_timestamp %at author_date_iso_8601 %ai committer_email %ce' | paste -d " " - - - | tail -r | awk '{
+git log --all --no-merges --shortstat --pretty=format:'commit_hash %H commit_hash_abbreviated %h tree_hash %T tree_hash_abbreviated %t parent_hashes %P parent_hashes_abbreviated %p author_email %ae author_date %ad author_date_unix_timestamp %at author_date_iso_8601 %ai committer_email %ce' | paste -d " " - - - | tail -r | awk -v q='"' -v c=':' -v cl='{' -v cr='}' -v bl='[' -v br=']' -v e=',' -v t='    ' '
+BEGIN {
+	print cl;
+	print t q "commits" q c bl;
+}
+{
 	count++;
-	print "commit_nr " count;
-	print "commit_hash " $2;
-	print "commit_hash_abbreviated " $4;
-	print "tree_hash " $6;
-	print "tree_hash_abbreviated " $8;
-	if ( count == 1 ) {
-		print "parent_hashes " "";
-		print "parent_hashes_abbreviated " "";
-		print "author_email " $12;
-		print "date_day_week " $14;
-		print "date_month_name " $15;
-		print "date_month_day " $16;
-		print "date_hour " $17;
-		print "date_year " $18;
-		print "date_hour_gmt " $19;
-		print "author_date_unix_timestamp " $21;
-		print "date_iso_8601 " $23;
-		print "committer_email " $27;
-		print "files_changed " $28;
-		print "insertions " $31;
-		print "deletions " $33;
-		print "impact " $31 - $33;
-	} else {
-		print "parent_hashes " $10;
-		print "parent_hashes_abbreviated " $12;
-		print "author_email " $14;
-		print "date_day_week " $16;
-		print "date_month_name " $17;
-		print "date_month_day " $18;
-		print "date_hour " $19;
-		print "date_year " $20;
-		print "date_hour_gmt " $21;
-		print "author_date_unix_timestamp " $23;
-		print "date_iso_8601 " $25;
-		print "committer_email " $29;
-		print "files_changed " $30;
-		print "insertions " $33;
-		print "deletions " $35;
-		print "impact " $33 - $35;
+	print t t cl;
+	print t t t q "commit_nr" q c q count q e;
+	print t t t q "commit_hash" q c q $2 q e;
+	print t t t q "commit_hash_abbreviated" q c q $4 q e;
+	print t t t q "tree_hash" q c q $6 q e;
+	print t t t q "tree_hash_abbreviated" q c q $8 q e;
+    if ( count == 1 ) {
+		print t t t q "parent_hashes" q c q "" q e;
+		print t t t q "parent_hashes_abbreviated" q c q "" q e;
+		print t t t q "author_email" q c q $12 q e;
+		print t t t q "date_day_week" q c q $14 q e;
+		print t t t q "date_month_name" q c q $15 q e;
+		print t t t q "date_month_day" q c q $16 q e;
+		print t t t q "date_hour" q c q $17 q e;
+		print t t t q "date_year" q c q $18 q e;
+		print t t t q "date_hour_gmt" q c q $19 q e;
+		print t t t q "author_date_unix_timestamp" q c q $21 q e;
+		print t t t q "date_iso_8601" q c q $23 q e;
+		print t t t q "committer_email" q c q $27 q e;
+		print t t t q "files_changed" q c q $28 q e;
+		print t t t q "insertions" q c q $31 q e;
+		print t t t q "deletions" q c q $33 q e;
+		print t t t q "impact" q c q $31 - $33 q;
+    } else {
+		print t t t q "parent_hashes" q c q $10 q e;
+		print t t t q "parent_hashes_abbreviated" q c q $12 q e;
+		print t t t q "author_email" q c q $14 q e;
+		print t t t q "date_day_week" q c q $16 q e;
+		print t t t q "date_month_name" q c q $17 q e;
+		print t t t q "date_month_day" q c q $18 q e;
+		print t t t q "date_hour" q c q $19 q e;
+		print t t t q "date_year" q c q $20 q e;
+		print t t t q "date_hour_gmt" q c q $21 q e;
+		print t t t q "author_date_unix_timestamp" q c q $23 q e;
+		print t t t q "date_iso_8601" q c q $25 q e;
+		print t t t q "committer_email" q c q $29 q e;
+		print t t t q "files_changed" q c q $30 q e;
+		print t t t q "insertions" q c q $33 q e;
+		print t t t q "deletions" q c q $35 q e;
+		print t t t q "impact" q c q $33 - $35 q;
 	}
-	print "";
+	print t t cr e;
+} END {
+	print t br;
+	print cr;
 }' > gitlog.json
