9cacc3ffa24c41b96c8bb8eb7069a8ce030d2f43
dreamyguy <i@dreamyguy.com>
2016-12-04 23:33:54 +0100

    Prevent 'illegal byte sequence' errors by converting 'ISO-8859-1' characters to 'UTF-8' - Fixes #12

--- scripts/output-intermediate-gitlog.sh
+++ scripts/output-intermediate-gitlog.sh
@@ -15,6 +15,7 @@ dir=$1
 cd $dir &&
         echo -e "${Whi}Outputting ${Pur}${PWD##*/}${RCol}" >&2 &&
         git log --all --no-merges --shortstat --reverse --pretty=format:'commits\trepository\t'"${PWD##*/}"'\tcommit_hash\t%H\tcommit_hash_abbreviated\t%h\ttree_hash\t%T\ttree_hash_abbreviated\t%t\tparent_hashes\t%P\tparent_hashes_abbreviated\t%p\tauthor_name\t%an\tauthor_name_mailmap\t%aN\tauthor_email\t%ae\tauthor_email_mailmap\t%aE\tauthor_date\t%ad\tauthor_date_RFC2822\t%aD\tauthor_date_relative\t%ar\tauthor_date_unix_timestamp\t%at\tauthor_date_iso_8601\t%ai\tauthor_date_iso_8601_strict\t%aI\tcommitter_name\t%cn\tcommitter_name_mailmap\t%cN\tcommitter_email\t%ce\tcommitter_email_mailmap\t%cE\tcommitter_date\t%cd\tcommitter_date_RFC2822\t%cD\tcommitter_date_relative\t%cr\tcommitter_date_unix_timestamp\t%ct\tcommitter_date_iso_8601\t%ci\tcommitter_date_iso_8601_strict\t%cI\tref_names\t%d\tref_names_no_wrapping\t%D\tencoding\t%e\tsubject\t%s\tsubject_sanitized\t%f\tcommit_notes\t%N\tstats\t' |
+          iconv -f ISO-8859-1 -t UTF-8 |    # convert ISO-8859-1 encoding to UTF-8
           sed '/^[ \t]*$/d' |               # remove all newlines/line-breaks, including those with empty spaces
           tr '\n' 'ò' |                     # convert newlines/line-breaks to a character, so we can manipulate it without much trouble
           tr '\r' ' ' |                     # replace carriage returns with a space, so we avoid new lines popping from placeholders that allow user input
