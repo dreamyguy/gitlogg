fd0584acfe2061c3269d2b786e77a2751e5368f7
Andrew Bradley <cspotcode@gmail.com>
2016-12-05 16:40:12 -0500

    Fixes quotes in magic __dirname incantation

--- scripts/gitlogg.sh
+++ scripts/gitlogg.sh
@@ -3,7 +3,7 @@
 # Magical incantation mimics node's __dirname
 __dirname="$(cd "$(
     dirname "$(
-        node -e "var out=process.argv[1];try {out=require('path').resolve(out, '..', require('fs').readlinkSync(out))} catch(e) {} finally {console.log(out)}" "${BASH_SOURCE[0]}"
+        node -e 'var out=process.argv[1];try {out=require("path").resolve(out, "..", require("fs").readlinkSync(out))} catch(e) {} finally {console.log(out)}' "${BASH_SOURCE[0]}"
     )"
 )" && pwd)"
 
